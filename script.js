2366152a3548391455cdd7f6f3f34791f2ed420e
Suhit gupta <suhitgupta111@gmail.com>
2021-06-21 17:12:49 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 'use strict';
+
+let score0El = document.querySelector('#score--0');
+let score1El = document.getElementById('score--1'); // getElementById can only be used to call by id and where as query selector can be used for both class abd selector
